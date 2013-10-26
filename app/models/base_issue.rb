26a30fe5ecf93d1f84a83ee0e57ea989aa08f206
Ubuntu <ubuntu@ip-10-133-152-121.ap-northeast-1.compute.internal>
2013-10-26 00:48:48 +0000

    db migrate.

--- app/models/base_issue.rb
+++ app/models/base_issue.rb
@@ -1,3 +1,13 @@
 class BaseIssue < ActiveRecord::Base
-  attr_accessible :baseCurrency, :date, :duration, :endDate, :exchangeRate, :interestRate, :name, :noItem, :principalForeign, :principalJPY, :status, :valueForeign, :valueJPY
+  attr_accessible :baseCurrency, :name, :principalForeign, :principalJPY, :status
 end
+
+class FixIssue < BaseIssue
+  attr_accessible :date, :endDate, :valueJPY,:valueForeign, :exchangeRate, :interestRate, :duration
+end
+
+class UnfixIssue < BaseIssue
+  attr_accessible :noItem
+end
+
+
--- db/migrate/20131026003526_add_type_to_base_issuetype:string.rb
+++
@@ -1,4 +0,0 @@
-class AddTypeToBaseIssuetype:string < ActiveRecord::Migration
-  def change
-  end
-end
