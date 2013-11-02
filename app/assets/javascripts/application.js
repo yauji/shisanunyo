52c779f54fa412018cdd9199888d3d78996ab9da
Ubuntu <ubuntu@ip-10-133-147-93.ap-northeast-1.compute.internal>
2013-11-02 10:12:01 +0000

    made fix issue's form.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,56 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+//	$("tr").css("color", "green");
+	// $("#fi_formbody > *").css("color", "red");
+	$("#fi_formbody > *").css("display", "none");
+	  //  	$("#fi_trName").css("display", "none");
+	
+    $("#fi_requestType").change(function(){
+	$("#fi_formbody > *").css("display", "none");
+
+	$("#fi_trName").css("display", "table-row");
+	$("#fi_trBaseCurrency").css("display", "table-row");
+	$("#fi_trDate").css("display", "table-row");
+
+    	if($("#fi_requestType").val() == "jpy2fc"){
+	$("#fi_trPrincipalJPY").css("display", "table-row");
+	$("#fi_trValueForeign").css("display", "table-row");
+	$("#fi_trExchangeRate").css("display", "table-row");
+
+    	}else if($("#fi_requestType").val() == "fc2jpy"){
+	$("#fi_trPrincipalForeign").css("display", "table-row");
+	$("#fi_trValueJPY").css("display", "table-row");
+	$("#fi_trExchangeRate").css("display", "table-row");
+    		
+    	}else if($("#fi_requestType").val() == "teiki_jpy"){
+	$("#fi_trPrincipalJPY").css("display", "table-row");
+	$("#fi_trInterestRate").css("display", "table-row");
+	$("#fi_trDuration").css("display", "table-row");
+    		
+    	}else if($("#fi_requestType").val() == "teiki_fc"){
+	$("#fi_trPrincipalForeign").css("display", "table-row");
+	$("#fi_trInterestRate").css("display", "table-row");
+	$("#fi_trDuration").css("display", "table-row");
+    		
+    	}else if($("#fi_requestType").val() == "shikumi_jpy2fc"){
+	$("#fi_trPrincipalJPY").css("display", "table-row");
+	$("#fi_trExchangeRate").css("display", "table-row");
+    		
+    	}else if($("#fi_requestType").val() == "shikumi_fc2jpy"){
+	$("#fi_trPrincipalForeign").css("display", "table-row");
+	$("#fi_trExchangeRate").css("display", "table-row");
+    		
+    	}else if($("#fi_requestType").val() == "shikumi_fc2fc"){
+	$("#fi_trPrincipalForeign").css("display", "table-row");
+	$("#fi_trExchangeRate").css("display", "table-row");
+    		
+    	}else{
+    		
+    	}
+    	
+    });
+});
+
