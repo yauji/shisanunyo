07b672b18b9f0e9286e30bf87eee0506581a2146
Ubuntu <ubuntu@ip-10-160-6-146.ap-northeast-1.compute.internal>
2013-12-29 13:01:47 +0000

    modifing application.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,8 +39,8 @@ $(function(){
 	$("#fi_trDate").css("display", "table-row");
 	$("#fi_trMemo").css("display", "table-row");
 	
-			$("#fix_issue_principalCurrency").removeAttr("disabled");
-			$("#fix_issue_baseCurrency").removeAttr("disabled");
+			// $("#fix_issue_principalCurrency").removeAttr("disabled");
+			// $("#fix_issue_baseCurrency").removeAttr("disabled");
 
 
     	if($("#fi_requestType").val() == "jpy2fc"){
@@ -48,29 +48,29 @@ $(function(){
 	$("#fi_trValueForeign").css("display", "table-row");
 	$("#fi_trExchangeRate").css("display", "table-row");
 	
-			$("#fi_trPrincipalCurrency").val("JPY");
-			$("#fix_issue_principalCurrency").attr("disabled", true);
+			$("#fix_issue_principalCurrency").val("JPY");
+			// $("#fix_issue_principalCurrency").attr("disabled", true);
 			$("#fix_issue_baseCurrency").val("AUD");
 
 	
     	}else if($("#fi_requestType").val() == "fc2jpy"){
-	$("#fi_trPrincipalForeign").css("display", "table-row");
-	$("#fi_trValueJPY").css("display", "table-row");
-	$("#fi_trExchangeRate").css("display", "table-row");
+			$("#fi_trPrincipalForeign").css("display", "table-row");
+			$("#fi_trValueJPY").css("display", "table-row");
+			$("#fi_trExchangeRate").css("display", "table-row");
 	
 			$("#fix_issue_baseCurrency").val("JPY");
-			$("#fix_issue_baseCurrency").attr("disabled", true);
+			// $("#fix_issue_baseCurrency").attr("disabled", true);
 
     		
     	}else if($("#fi_requestType").val() == "teiki_jpy"){
-	    $("#fi_trPrincipalJPY").css("display", "table-row");
-	    $("#fi_trInterestRate").css("display", "table-row");
-	    $("#fi_trDuration").css("display", "table-row");
+	    	$("#fi_trPrincipalJPY").css("display", "table-row");
+	    	$("#fi_trInterestRate").css("display", "table-row");
+	    	$("#fi_trDuration").css("display", "table-row");
 	
-	    $("#fi_trPrincipalCurrency").val("JPY");
-	    $("#fix_issue_principalCurrency").attr("disabled", true);
-	    $("#fix_issue_baseCurrency").val("JPY");
-	    $("#fix_issue_baseCurrency").attr("disabled", true);
+	    	$("#fix_issue_principalCurrency").val("JPY");
+	    	// $("#fix_issue_principalCurrency").attr("disabled", true);
+	    	$("#fix_issue_baseCurrency").val("JPY");
+	    	// $("#fix_issue_baseCurrency").attr("disabled", true);
 
     		
     	}else if($("#fi_requestType").val() == "teiki_fc"){
@@ -109,8 +109,8 @@ $(function(){
 		$("#ui_trPrincipalJPY").css("display", "table-row");
 		$("#ui_trBasicPrice").css("display", "table-row");
 		
-		$("#unfix_issue_principalCurrency").attr("disabled", true);
-		$("#unfix_issue_baseCurrency").attr("disabled", true);
+		// $("#unfix_issue_principalCurrency").attr("disabled", true);
+		// $("#unfix_issue_baseCurrency").attr("disabled", true);
 
 	
     	$("#ui_requestType").change(function(){
@@ -123,8 +123,8 @@ $(function(){
 		$("#ui_trDate").css("display", "table-row");
 		$("#ui_trNoItem").css("display", "table-row");
 	
-		$("#unfix_issue_principalCurrency").removeAttr("disabled");
-		$("#unfix_issue_baseCurrency").removeAttr("disabled");
+		// $("#unfix_issue_principalCurrency").removeAttr("disabled");
+		// $("#unfix_issue_baseCurrency").removeAttr("disabled");
 
 
     	if($("#ui_requestType").val() == "jpy"){
@@ -132,9 +132,9 @@ $(function(){
 			$("#ui_trBasicPrice").css("display", "table-row");
 	
 			$("#unfix_issue_principalCurrency").val("JPY");
-			$("#unfix_issue_principalCurrency").attr("disabled", true);
+			// $("#unfix_issue_principalCurrency").attr("disabled", true);
 			$("#unfix_issue_baseCurrency").val("JPY");
-			$("#unfix_issue_baseCurrency").attr("disabled", true);
+			// $("#unfix_issue_baseCurrency").attr("disabled", true);
 
 	
     	}else if($("#ui_requestType").val() == "fc"){
