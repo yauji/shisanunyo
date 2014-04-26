7f8c9236b6681dbbfab6fbd357911eb633e2f82f
Ubuntu <ubuntu@ip-10-152-178-15.ap-northeast-1.compute.internal>
2014-04-26 09:34:23 +0000

    modi for basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  http_basic_authenticate_with name:ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
   
   module IssueStatus
     ACTIVE = 'active'
